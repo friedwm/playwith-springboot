cdad7c47dea4c29895da220b695800df2ca713bc
quxiao <quxiao@meitunmama.com>
2019-05-30 19:30:02 +0800

    实现

--- src/main/java/xyz/quxiao/playwithspringboot/PlaywithSpringbootApplication.java
+++ src/main/java/xyz/quxiao/playwithspringboot/PlaywithSpringbootApplication.java
@@ -1,11 +1,15 @@
 package xyz.quxiao.playwithspringboot;
 
+import cn.hutool.http.HtmlUtil;
 import cn.hutool.poi.excel.ExcelReader;
 import cn.hutool.poi.excel.ExcelUtil;
 import cn.hutool.poi.excel.ExcelWriter;
 import java.io.File;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.apache.commons.collections4.CollectionUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +17,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlaywithSpringbootApplication implements ApplicationRunner {
+
+  private static final Logger logger = LoggerFactory.getLogger(PlaywithSpringbootApplication.class);
 
   public static void main(String[] args) {
     SpringApplication.run(PlaywithSpringbootApplication.class, args);
@@ -25,8 +31,21 @@ public class PlaywithSpringbootApplication implements ApplicationRunner {
       String file = files.get(0);
       String fileName = file.substring(0, file.lastIndexOf("."));
       String newName = fileName + "_clean.xls";
-
       ExcelReader reader = ExcelUtil.getReader(new File(file));
+      List<List<Object>> rows = reader.read();
+      ExcelWriter writer = ExcelUtil.getWriter(newName);
+      logger.info("file:{}, dest:{}", file, newName);
+      CollectionUtils.emptyIfNull(rows).stream().forEach(row -> {
+        List<String> newRow = row.stream().map(c -> {
+          return HtmlUtil.unescape(String.valueOf(c));
+        }).collect(Collectors.toList());
+
+        writer.writeRow(newRow);
+      });
+
+      writer.close();
+      reader.close();
+
     }
   }
 }
